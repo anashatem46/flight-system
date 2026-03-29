4f6d8561d7f417a8eb26f048f77bd7a8fbec7871
Anas Hatem <74430931+anashatem46@users.noreply.github.com>
2026-03-29 04:01:41 +0200

    initialize flight booking system with database schema, application configuration, and basic structure

---
+++ src/main/java/org/learnjava/flightsystem/flightbooking/Api/FlightBookingController.java
@@ -0,0 +1,10 @@
+package org.learnjava.flightsystem.flightbooking.Api;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/booking")
+public class FlightBookingController {
+}
---
+++ src/main/java/org/learnjava/flightsystem/flightbooking/entity/Booking.java
@@ -0,0 +1,47 @@
+package org.learnjava.flightsystem.flightbooking.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+
+@Data
+@Entity
+@Table(name = "bookings")
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "bookings_id_gen")
+    @SequenceGenerator(name = "bookings_id_gen", sequenceName = "bookings_booking_id_seq", allocationSize = 1)
+    @Column(name = "booking_id", nullable = false)
+    private Integer id;
+
+    @Column(name = "booking_time")
+    private Instant bookingTime = Instant.now() ;
+
+    @Column(name = "status", nullable = false, length = 20)
+    private String status;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private Instant createdAt = Instant.now();
+
+
+    @CreationTimestamp
+    @Column(name = "updated_at")
+    private Instant updatedAt = Instant.now();
+
+    @Column(name = "flight_id", nullable = false)
+    private Integer flightId;
+
+    @Column(name = "user_id", nullable = false)
+    private Integer userId;
+}
+
+
+
+
