03121e2183a7e5a9f09db21184b74bb5ad671218
Anas Hatem <74430931+anashatem46@users.noreply.github.com>
2026-04-26 22:05:37 +0300

    refactor pom.xml to update groupId and restructure modules

--- src/main/java/org/learnjava/flightsystem/flightbooking/exception/BookingApiException.java
+++
@@ -1,8 +0,0 @@
-package org.learnjava.flightsystem.flightbooking.exception;
-
-public class BookingApiException extends RuntimeException
-{
-    public BookingApiException(String message){
-        super(message);
-    }
-}
